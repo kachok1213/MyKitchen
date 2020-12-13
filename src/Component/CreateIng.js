8f6820a1338f81bbea545cc60def1cf8ccc3e496
oryan_mamane <oryanmamane@Gmail.com>
2020-12-13 18:51:10 +0200

    oryan-13-12

--- src/Component/CreateIng.js
+++ src/Component/CreateIng.js
@@ -1,75 +1,96 @@
-import React, { Component } from 'react';
-import {Form,Button} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
-
-
-const ingridients = []
+const ingridients = [];
+let apiing = "https://localhost:44369/api/Ingredient/";
 
 export default class CreateIng extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            name:'',
-            img:'',
-            calories:''
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      image: "",
+      calories: "",
+    };
+  }
 
-       
-    }
+  handleChanged = (item) => {
+    this.setState({ [item.target.name]: item.target.value });
+  };
 
-}
-
-handleChanged = (item) =>{
-    this.setState({[item.target.name]:item.target.value})
-    
-}
-
-
-
-submit = () =>{
-    
+  submit = () => {
     let data = {
-        name:this.state.name,
-        img:this.state.img,
-        calories:this.state.calories
+      name: this.state.name,
+      image: this.state.image,
+      calories: this.state.calories,
+    };
+    ingridients.push(data);
+    console.log(ingridients);
 
-    }
-    ingridients.push(data)
-    console.log(this.state)
-    console.log(ingridients)
-    
-    
-}
+    fetch(apiing, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-type": "application/json; charset=UTF-8", //very important to add the 'charset=UTF-8'!!!!
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then(
+        (result) => {
+          if (result === 1) {
+            alert("Ingredient added");
+          }
+        },
+        (error) => {
+          console.log("err post=", error);
+        }
+      );
+  };
 
   render() {
-
     return (
-
-        <div> 
+      <div>
         <Form>
-        <h1>Create Ing</h1>
-<Form.Group controlId="formBasicName" >
-  <Form.Label>Name</Form.Label>
-  <Form.Control type="Text" placeholder="Enter Recpie Name" value = {this.state.name} name = "name" onChange={this.handleChanged}/>
-</Form.Group>
-<Form.Group controlId="formBasicCm">
-  <Form.Label>Image</Form.Label>
-  <Form.Control type="Text" placeholder="Password" value = {this.state.img} name = "img"  onChange={this.handleChanged}/>
-  </Form.Group>
-  <Form.Group controlId="formTime">
-  <Form.Label>Calories</Form.Label>
-  <Form.Control type="Text" placeholder="Password" value = {this.state.calories} name = "calories" onChange={this.handleChanged} />
-  </Form.Group>
-  
-<Button variant="primary" type="button" onClick={this.submit}>
-  Submit
-</Button>
-<Button variant="primary" >
-  Submit
-</Button>
-          </Form>
-        
-         </div>
+          <h1>Create Ing</h1>
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="Text"
+              placeholder="Enter Recpie Name"
+              value={this.state.name}
+              name="name"
+              onChange={this.handleChanged}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicCm">
+            <Form.Label>Image</Form.Label>
+            <Form.Control
+              type="Text"
+              placeholder="Password"
+              value={this.state.image}
+              name="image"
+              onChange={this.handleChanged}
+            />
+          </Form.Group>
+          <Form.Group controlId="formTime">
+            <Form.Label>Calories</Form.Label>
+            <Form.Control
+              type="Text"
+              placeholder="Password"
+              value={this.state.calories}
+              name="calories"
+              onChange={this.handleChanged}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="button" onClick={this.submit}>
+            Submit
+          </Button>
+          <Button variant="primary">Submit</Button>
+        </Form>
+      </div>
     );
   }
 }
-
